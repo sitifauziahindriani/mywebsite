877a8c91718343894d0e018a4ecdfee5e7af10a6
sitifauziahindriani <fauziahsiti1410@gmail.com>
2020-10-07 10:10:23 +0700

    update nafbar

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 alert('selamat datang di website saya...')
+const showModal = () => {
+
+    //kita ambil elmen yg akan di SHOW / tampilkan
+    let modal = document.querySelector('.modalMenu')
+    // kita atur style jadi dari elment tersebut
+    //jadikan dia terlihat
+    modal.style.display = 'flex'
+
+}
+
+// fungsi untuk hide elment
+const hideModal = () => {
+    let modal = document.querySelector('.modalMenu')
+    // kita atur style jadi dari elment tersebut
+    //jadikan dia terlihat
+    modal.style.display = 'none'
+
+
+}
+
+const hideaboutAlert = () => {
+    let aboutAlert = document.querySelector('.aboutAlert')
+    aboutAlert.style.display = 'none'
+}
